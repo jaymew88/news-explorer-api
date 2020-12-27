671701dc5d7e78a9b33e1931cf3e62b41e03fee9
Jayme Waye <jaymewaye@gmail.com>
2020-12-27 16:08:06 -0500

    error message syntax fix

--- config/errors/errorMessages.js
+++ config/errors/errorMessages.js
@@ -1,10 +1,10 @@
 const errorMessages = {
-  authRequired = 'Authorization Required',
-  couldNotCreateArticle = 'Article validation failed',
-  notUsersArticle = 'Article does not belong to user',
-  invalidArticle = 'Invalid article',
-  invalidUser = 'User ID not found',
-  incorrectLogin = 'Incorrect password or email',
+  authRequired: 'Authorization Required',
+  couldNotCreateArticle: 'Article validation failed',
+  notUsersArticle: 'Article does not belong to user',
+  invalidArticle: 'Invalid article',
+  invalidUser: 'User ID not found',
+  incorrectLogin: 'Incorrect password or email',
 
 
 }
